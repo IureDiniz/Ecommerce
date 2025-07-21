39de61df558020a7e8661dd909523d0c832f80ed
IureDiniz <iuredinizsoares@gmail.com>
2025-07-21 10:47:53 -0300

    Adicionar pedido ao banco pela TerminalGUI

---
+++ Ecommerce/src/br/com/eccomerce/aplication/Main.java
@@ -0,0 +1,11 @@
+package br.com.eccomerce.aplication;
+
+import br.com.eccomerce.gui.TerminalGUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TerminalGUI.menu();
+	}
+	
+}
