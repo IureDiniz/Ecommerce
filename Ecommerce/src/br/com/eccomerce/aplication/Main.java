f7aa287730251d5081189f3d55e86f4d0bb6e014
IureDiniz <iuredinizsoares@gmail.com>
2025-07-29 17:51:45 -0300

    Importação do GUI

--- Ecommerce/src/br/com/eccomerce/aplication/Main.java
+++ Ecommerce/src/br/com/eccomerce/aplication/Main.java
@@ -1,11 +1,14 @@
 package br.com.eccomerce.aplication;
 
 import br.com.eccomerce.gui.TerminalGUI;
+import br.com.eccomerce.gui.gui.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		TerminalGUI.menu();
+		//TerminalGUI.menu();
+		
+		TelaPrincipal.main(args);
 	}
 	
 }
